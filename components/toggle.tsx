4f75e5958b558856f4ed8db28a77aae85bd750fe
CharlesGarth <charlesgarth@live.co.uk>
2022-10-20 13:00:07 +0100

    dark mode icons as SVG rather than unicode

--- components/toggle.tsx
+++ components/toggle.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export const Toggle: React.FC = () => {
     const [mounted, setMounted] = useState<boolean>();
@@ -13,16 +14,30 @@ export const Toggle: React.FC = () => {
         return null;
     }
 
-    const SUN = '☼';
-    const MOON = '☽';
+    const Sun = (
+        <Image
+            src="/assets/icons/sun.svg"
+            width={35}
+            height={39}
+            alt="a sun icon"
+        />
+    );
+    const Moon = (
+        <Image
+            src="/assets/icons/moon.svg"
+            width={35}
+            height={39}
+            alt="a moon icon"
+        />
+    );
     return (
         <>
             <div
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
                 className="fixed bottom-4 bg-darkness dark:bg-white z-20 w-10 h-10 rounded-r-full cursor-pointer text-center"
             >
-                <span className="inline-block p-[6px] select-none align-middle text-xl text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-blue-500 hover:from-red-400 hover:to-blue-400">
-                    {theme === 'dark' ? SUN : MOON}
+                <span className="select-none flex justify-center">
+                    {theme === 'dark' ? Sun : Moon}
                 </span>
             </div>
         </>
